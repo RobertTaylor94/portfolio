8f3991bf8aeab70491136ec95b5013214e53c24b
Robert Taylor <rjt903@gmail.com>
2023-03-06 17:15:24 +0200

    Updated imports for new file locations

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import NavTabs from "./components/NavTabs";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Contact from "./components/Contact/Contact";
+import Home from "./components/Home/Home";
+import Gallery from "./components/Gallery/Gallery";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
+        <NavBar />
         {/* Wrap route elements in Routes component */}
         <Routes>
           {/* Default route for home page */}
           <Route path="/" element={<Home />} />
+          {/* default route for home when viewed on deployed GitHub Pages site */}
+          {/* <Route path="/portfolio" element={<Home />} /> */}
           <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
         </Routes>
       </div>
     </Router>
