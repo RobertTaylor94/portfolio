ca256f13ae17ef3c49e92f2de8054142682edd74
Robert Taylor <rjt903@gmail.com>
2023-03-06 20:01:54 +0200

    Started building image gallery with MUI ImageList

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import images from './images.json'
+import { Box, ImageList, ImageListItem } from '@mui/material'
 
 function Gallery() {
     return (
-        <div>
-            Gallery
-        </div>
+        <Box sx={{ overflowY: 'scroll', width: '100%', height: '100%', justifyContent: 'center', display: 'flex'}}>
+            <ImageList variant='masonry' cols={3} gap={8} sx={{ }}>
+                {images.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img 
+                            src={item.img}
+                            alt={item.title} 
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList> 
+        </Box>
     );
 }
 
