9caf178dd39c60a96baafea83fe54f4c33eaaa9f
Robert Taylor <rjt903@gmail.com>
2023-03-07 20:53:36 +0200

    Added hero banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Skeleton, Box } from '@mui/material';
 import './home.css';
+// import banner from '/images/banner.jpeg';
 
 function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <Box component='img' src={process.env.PUBLIC_URL + '/images/banner.jpeg'} sx={{ height: '60vh', objectFit: 'cover', width: '100vw', overflow: 'hidden' }}>
+        </Box>
     );
 }
 
