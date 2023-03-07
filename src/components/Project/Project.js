2b62e0a5bd50ff353c6da8f0ed391674ad037bee
Robert Taylor <rjt903@gmail.com>
2023-03-07 22:12:22 +0200

    mock-up card made for displaying projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './project.css';
+import '../../projects.json'
+import { Card, CardMedia, CardActions, Button, CardContent, Typography, Link } from '@mui/material';
 
 function Project() {
     return (
-        <div>
-            <div className="card">
-                <div className="card-header">
-                    Project One
-                </div>
-                <div className="card-body">
-                    <img alt="" src=""></img>
-                    Body
-                </div>
-            </div>
-        </div>
+        <Card>
+            <CardMedia sx={{  }} image="" title="" />
+            <CardContent>
+                <Typography gutterBottom variant="h3">App Title</Typography>
+            </CardContent>
+            <CardActions>
+                <Link component='button' size='small' href=''>GitHub</Link>
+                <Link component='button' size='small' href=''>Deployed Site</Link>
+            </CardActions>
+        </Card>
     );
 }
 
