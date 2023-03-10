5806871ef4e67d900f23e7b4d2a4b7538451443a
Robert Taylor <rjt903@gmail.com>
2023-03-10 11:43:58 +0200

    Added form for contacting me on contact site and links to GitHub and LinkedIn

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React from "react";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  makeStyles,
+} from "@mui/material";
 
 function Contact() {
-    return (
-        <div>
-            Contact
-        </div>
-    );
+  return (
+    <Container>
+      <Typography variant="h4" align="center" marginTop={3} marginBottom={3}>
+        Contact Me
+      </Typography>
+      <form method="POST" name="contactForm" netlify netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contactForm" />
+        <TextField id="name" label="Name" fullWidth margin="normal" />
+        <TextField id="email" label="Email" fullWidth margin="normal" />
+        <TextField
+          id="message"
+          label="Message"
+          fullWidth
+          multiline
+          rows={4}
+          margin="normal"
+        />
+        <Button variant="contained" color="primary" fullWidth>
+          Submit
+        </Button>
+      </form>
+      <Grid container spacing={2} marginTop={3}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom>
+            Contact Details
+          </Typography>
+          <Typography gutterBottom>
+            Email:{" "}
+            <a href="mailto:your.email@example.com">your.email@example.com</a>
+          </Typography>
+          <Typography gutterBottom>Phone: +1 (555) 555-5555</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom>
+            Links
+          </Typography>
+          <Typography gutterBottom>
+            <a href="https://github.com/your-github-username">GitHub</a>
+          </Typography>
+          <Typography gutterBottom>
+            <a href="https://www.linkedin.com/in/your-linkedin-username/">
+              LinkedIn
+            </a>
+          </Typography>
+          <Typography gutterBottom>
+            <a href="/path/to/your/resume.pdf" download>
+              Download Resume
+            </a>
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
+
 export default Contact;
